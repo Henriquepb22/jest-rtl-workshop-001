5a80b9c91b21814a04ffd2a99e0206dc3b38c6c6
MarcoBrito <marco_monteiro@live.com>
2022-06-26 15:43:10 -0300

    Button tests

--- client/src/unit/components/Button/index.test.tsx
+++ client/src/unit/components/Button/index.test.tsx
@@ -1,16 +1,54 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Button from './index'
 
 describe('<Button />', () => {
-  /*
-    O Primeiro teste é recomendado ser sempre a variação 'padrão' do seu módulo,
-    sendo passado o minimo de props necessária para sua renderização
+  it('should render component', () => {
+    const { container } = render(<Button />)
 
-    Dica: Ao finalizar a criação do componente, adicione um teste de snapshot no
-    teste das propriedades padrões para garantir que qualquer alteração no componente
-    o teste falhe
-  */
-  it.todo('should render the medium size be default')
-  it.todo('should render the small button variant')
-  it.todo('should render the big button variant')
-  it.todo('should render a disabled button')
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render the medium size be default', () => {
+    render(<Button />)
+
+    expect(screen.getByRole('button')).toHaveClass('px-4 py-2 text-base')
+  })
+
+  it('should render the primary color be default', () => {
+    render(<Button />)
+
+    expect(screen.getByRole('button')).toHaveClass('bg-pink-600 text-white')
+  })
+
+  it('should render the secondary color', () => {
+    render(<Button color="secondary" />)
+
+    expect(screen.getByRole('button')).toHaveClass('bg-gray-300 text-black')
+  })
+
+  it('should render the small button variant', () => {
+    render(<Button size="sm" />)
+
+    expect(screen.getByRole('button')).toHaveClass('px-2 py-1 text-sm')
+  })
+
+  it('should render the big button variant', () => {
+    render(<Button size="lg" />)
+
+    expect(screen.getByRole('button')).toHaveClass('px-6 py-3 text-lg')
+  })
+
+  it('should render a disabled button', () => {
+    render(<Button disabled />)
+
+    expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  it('should render a hover opacity', () => {
+    render(<Button disabled />)
+
+    userEvent.hover(screen.getByRole('button'))
+    expect(screen.getByRole('button')).toHaveClass('hover:opacity-80')
+  })
 })
