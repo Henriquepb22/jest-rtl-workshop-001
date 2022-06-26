c2a7528521954d0bb355c8adba82bd373b4a25d5
MarcoBrito <marco_monteiro@live.com>
2022-06-26 15:47:06 -0300

    Included test

--- client/src/unit/components/Button/index.test.tsx
+++ client/src/unit/components/Button/index.test.tsx
@@ -4,51 +4,57 @@ import Button from './index'
 
 describe('<Button />', () => {
   it('should render component', () => {
-    const { container } = render(<Button />)
+    const { container } = render(<Button>Entrar</Button>)
 
     expect(container).toMatchSnapshot()
   })
 
   it('should render the medium size be default', () => {
-    render(<Button />)
+    render(<Button>Entrar</Button>)
 
     expect(screen.getByRole('button')).toHaveClass('px-4 py-2 text-base')
   })
 
   it('should render the primary color be default', () => {
-    render(<Button />)
+    render(<Button>Entrar</Button>)
 
     expect(screen.getByRole('button')).toHaveClass('bg-pink-600 text-white')
   })
 
   it('should render the secondary color', () => {
-    render(<Button color="secondary" />)
+    render(<Button color="secondary">Entrar</Button>)
 
     expect(screen.getByRole('button')).toHaveClass('bg-gray-300 text-black')
   })
 
   it('should render the small button variant', () => {
-    render(<Button size="sm" />)
+    render(<Button size="sm">Entrar</Button>)
 
     expect(screen.getByRole('button')).toHaveClass('px-2 py-1 text-sm')
   })
 
   it('should render the big button variant', () => {
-    render(<Button size="lg" />)
+    render(<Button size="lg">Entrar</Button>)
 
     expect(screen.getByRole('button')).toHaveClass('px-6 py-3 text-lg')
   })
 
   it('should render a disabled button', () => {
-    render(<Button disabled />)
+    render(<Button disabled>Entrar</Button>)
 
     expect(screen.getByRole('button')).toBeDisabled()
   })
 
   it('should render a hover opacity', () => {
-    render(<Button disabled />)
+    render(<Button>Entrar</Button>)
 
     userEvent.hover(screen.getByRole('button'))
     expect(screen.getByRole('button')).toHaveClass('hover:opacity-80')
+  })
+
+  it('should render a text children', () => {
+    render(<Button>Entrar</Button>)
+
+    expect(screen.getByRole('button')).toHaveTextContent(/entrar/i)
   })
 })
