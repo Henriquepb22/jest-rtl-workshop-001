b6b4e79575d8ed437ef7a4b091eb20f1ec233114
MarcoBrito <marco_monteiro@live.com>
2022-07-05 21:52:46 -0300

    Included test SignUpForm

--- client/src/unit/components/SignUpForm/index.test.tsx
+++ client/src/unit/components/SignUpForm/index.test.tsx
@@ -1,5 +1,12 @@
-import { screen } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import SignUpForm from './index'
 
 describe('<SignUpForm />', () => {
-  it.todo('should render the form correctly')
+  it('should render the form correctly', () => {
+    render(<SignUpForm />)
+
+    expect(
+      screen.getByRole('form', { name: /Formulário de cadastro/i })
+    ).toBeInTheDocument()
+  })
 })
