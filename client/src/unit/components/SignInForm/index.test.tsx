5b8c3abcf9ac690e7d4d432dab2935e91c46cfab
MarcoBrito <marco_monteiro@live.com>
2022-07-05 21:40:09 -0300

    Included tests SignInForm

--- client/src/unit/components/SignInForm/index.test.tsx
+++ client/src/unit/components/SignInForm/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SignInForm from './index'
 
 describe('<SignInForm />', () => {
@@ -16,8 +17,37 @@ describe('<SignInForm />', () => {
     ).toBeInTheDocument()
   })
 
-  // it('should fill and submit the form', () => {
+  it('should fill and submit the form', () => {
+    render(<SignInForm />)
 
-  // })
-  it.todo('should return error if field incorrects')
+    userEvent.type(screen.getByRole('textbox', { name: /usuário/i }), 'marco')
+    userEvent.type(screen.getByLabelText(/senha/i), '1234')
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }))
+
+    expect(screen.getByText(/carregando/i)).toBeInTheDocument()
+  })
+
+  it('should return error if field user incorrects', () => {
+    render(<SignInForm />)
+
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }))
+    expect(screen.getByText(/usuário obrigatório/i)).toBeInTheDocument()
+    userEvent.type(screen.getByRole('textbox', { name: /usuário/i }), 'mar')
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }))
+    expect(
+      screen.getByText(/usuário precisa ter no mínimo 4 caracteres/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should return error if field password incorrects', () => {
+    render(<SignInForm />)
+
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }))
+    expect(screen.getByText(/senha obrigatória/i)).toBeInTheDocument()
+    userEvent.type(screen.getByLabelText(/senha/i), '123')
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }))
+    expect(
+      screen.getByText(/senha precisa ter no mínimo 4 caracteres/i)
+    ).toBeInTheDocument()
+  })
 })
